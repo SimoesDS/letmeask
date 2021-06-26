61cbb599832922b6b9d6f072c291cf6c348e71f1
SimoesDS <simoes.sdgo@gmail.com>
2021-06-26 10:37:47 -0300

    created question screen

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,48 @@
+import { useParams } from 'react-router-dom';
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import '../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+
+  const roomId = params.id;
+  console.log(params);
+
   return (
-    <div>
-      <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={roomId} />
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu Login</button>.
+            </span>
+            <Button type="submit" disabled={!user}>
+              Enviar Pergunta
+            </Button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
