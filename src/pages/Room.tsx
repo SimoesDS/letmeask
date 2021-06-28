04e326907b818551fbe1e03fb0a0831db6cff2fe
SimoesDS <simoes.sdgo@gmail.com>
2021-06-27 21:01:02 -0300

    refactoring function to catch firebase issues

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,36 +1,13 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 import '../styles/room.scss';
-
-type FirebaseQuestion = Record<
-  string,
-  {
-    author: {
-      name: string;
-      avatar: string;
-    };
-    content: string;
-    isAnswered: string;
-    isHighlighted: string;
-  }
->;
-
-type QuestionType = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isAnswered: string;
-  isHighlighted: string;
-};
 
 type RoomParams = {
   roomId: string;
@@ -39,37 +16,9 @@ type RoomParams = {
 export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
-
-  const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<QuestionType[]>([]);
-  const [title, setTitle] = useState('');
-
   const roomId = params.roomId;
-
-  useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomId}`);
-
-    roomRef.on('value', (room) => {
-      const databaseRoom = room.val();
-      if (databaseRoom) {
-        const firebaseQuestions: FirebaseQuestion = databaseRoom.questions;
-        const parsedQuestions = Object.entries(firebaseQuestions ?? {}).map(
-          ([key, value]) => {
-            return {
-              id: key,
-              content: value.content,
-              author: value.author,
-              isAnswered: value.isAnswered,
-              isHighlighted: value.isHighlighted,
-            };
-          }
-        );
-
-        setTitle(databaseRoom.title);
-        setQuestions(parsedQuestions);
-      }
-    });
-  }, [roomId]);
+  const [newQuestion, setNewQuestion] = useState('');
+  const { questions, title } = useRoom(roomId);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
